268b9c89539cbe2ed9cfa6e395daac6fb819cb7c
RAIMA <raimaaggarwal2005@gmail.com>
2026-08-22 16:18:35 +0530

    Fix production MySQL port

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
-
+    DB_PORT = int(os.getenv("DB_PORT", "3306"))
     JWT_SECRET = os.getenv("JWT_SECRET")
 
     RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
